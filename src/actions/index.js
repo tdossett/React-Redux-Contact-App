f7c19352d6d3df6e6c93d4d11018c0646f96dc54
Tim Dossett <tdossett@dostekinc.net>
2018-01-19 12:32:10 -0800

    change baseURL

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,11 @@
 // src/actions/index.js
 
+// baseURL: 'http://localhost:3030' local development
+
 import axios from 'axios';
 
 export const client = axios.create({
-    baseURL: 'http://localhost:3030',
+    baseURL: 'https://morning-cove-87124.herokuapp.com',
     headers: {
         'Content-Type': 'application/json'
     }
