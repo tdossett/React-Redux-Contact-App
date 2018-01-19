a1fb1eb274a71a172b0bf0426d3aaed99f7c011c
Tim Dossett <tdossett@dostekinc.net>
2018-01-19 09:01:01 -0800

    updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,7 +30,7 @@ app.use(helmet());
 app.use(compress());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(favicon(path.join(app.get(__dirname, 'public'), 'favicon.ico')));
+app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
 // Host the public folder
 app.use('/', feathers.static(app.get('public')));
 
